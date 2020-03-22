1cbabb9a435eeb0b6dd1baa58e60a6cef6185c32
ThomasJensz <53294412+ThomasJensz@users.noreply.github.com>
2020-03-22 12:55:02 +1000

    Menu/UI Added, Intro Video, Timer Limit

---
+++ 3d environment/Assets/Scripts/Player/VideoRaycaster.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+public class VideoRaycaster : MonoBehaviour
+{
+
+    public VideoPlayer vp;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp.loopPointReached += Vp_loopPointReached;
+    }
+
+    private void Vp_loopPointReached(VideoPlayer source)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform != null)
+                {
+                    switch (hit.transform.gameObject.name)
+                    {
+                        case "Skip":
+                            print("Skip");
+                            SceneManager.LoadScene(2);
+                            break;
+                        default:
+                            print(hit.transform.gameObject.name);
+                            break; 
+                    }
+                }
+            }
+        }    
+    }
+}
+
---
+++ 3d environment/Assets/WaterScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroll : MonoBehaviour
+{
+
+    public float xSpeed = 0.01f;
+    public float ySpeed = 0.01f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xOffset = Time.time * xSpeed;
+        float yOffset = Time.time * ySpeed;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(xOffset, yOffset);
+
+    }
+}
