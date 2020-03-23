ffa5a8039e3b0229ab1127ad500589d0687bbffe
ShadowGim <jacobcampbell972@gmail.com>
2020-03-23 12:02:13 +1100

    Shadow hit box
    Developed a shadow hit box that draws a fake hitbox as the user is drawing to show them how big and where the hitbox will spawn

--- 3d environment/Assets/Scripts/Level Creator/Player/DrawHitBox.cs
+++ 3d environment/Assets/Scripts/Level Creator/Player/DrawHitBox.cs
@@ -9,6 +9,13 @@ public class DrawHitBox : MonoBehaviour
     private LayerMask layer_mask; 
     private Vector3 startPos;
     private Vector3 endPos;
+
+
+    // Shadow box is a temporory plane object whose dimensions will continiously change as the user draws a hitbox. It will show the user how big and where the hitbox will be drawn
+    private GameObject shadowBox;
+    private MeshFilter shadowFilter;
+    private MeshRenderer shadowRenderer;
+    private Mesh shadowMesh;
     
     // we want the entire plane object to be on the same z axis so we get it from the starting pos z and use that when generating our hitbox
     private float zAxis; 
@@ -30,13 +37,21 @@ public class DrawHitBox : MonoBehaviour
 
             RaycastHit hit;
 
+            // Create the shadowbox object which will show the user how the box is being drawn. It will continiously be updated while the user is holding down the fire button
+            // this object will be destroyed when the user releases the fire button
+            shadowBox = new GameObject("plane");
+            shadowFilter = shadowBox.AddComponent(typeof(MeshFilter)) as MeshFilter;
+            shadowRenderer = shadowBox.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+            shadowMesh = new Mesh(); 
+
+
+
             // get the pos of the start point and log them into the startPos variable. This will allow us calculate the distance we need to render the hitbox
             if (Physics.Raycast(ray, out hit))
             {
                 // Make the start position = inital set of Vector3 coords that the user clicks on
                 startPos = hit.point;
                 zAxis = startPos.z - 3f;
-                Debug.Log("start position is " + startPos);
             }
         }
 
@@ -53,15 +68,116 @@ public class DrawHitBox : MonoBehaviour
             {
                 // Make the start position = inital set of Vector3 coords that the user clicks on
                 endPos = hit.point;
-                Debug.Log("end position is " + endPos);
+                // show the user how the hitbox is being drawn in real time 
+                DrawShadowBox(); 
             }
 
         }
         if (Input.GetButtonUp("Fire1"))
         {
             // after the user has released the key create the hitbox
-            generatePlane(); 
+            generatePlane();
+            destroyShadow(); 
         }
+    }
+
+    private void destroyShadow()
+    {
+       Destroy(shadowBox);
+
+        //implement at a later date. Some floating text that says the hitbox has been created
+    }
+
+    private void DrawShadowBox()
+    {
+        // the height and width is set to whatever the last position is
+        float height = endPos.y;
+        float width = endPos.x;
+
+       
+        float startX = startPos.x;
+        float startY = startPos.y;
+        //  in order to draw a plane we need 4 vertices. with 4 verticies we can make two right handed triangles and it is with those two right handed triangles we can draw a plane
+
+
+
+        Vector3[] vertices;
+        int[] triangles;
+        // Because of a process called backwards culling we need to make sure we feed in the vertices in a clockwise motion
+        if (startY < height)
+        {
+
+
+            if (startX < width)
+            {
+                vertices = new Vector3[]
+               {
+                                // starting position of the vertices in world space
+                    new Vector3(startX,startY, zAxis),
+                    new Vector3(startX,height, zAxis),
+                    new Vector3(width,height,zAxis),
+                    new Vector3(width,startY,zAxis)
+               };
+
+
+            }
+            else
+            {
+                vertices = new Vector3[]
+                {
+
+                    new Vector3(startX,startY, zAxis),
+                    new Vector3(width,startY, zAxis),
+                    new Vector3(width,height,zAxis),
+                    new Vector3(startX,height,zAxis)
+
+                };
+
+
+            }
+
+
+        }
+        else
+        {
+            if (width > startX)
+            {
+                vertices = new Vector3[]
+                {
+                    new Vector3(startX,startY,zAxis),
+                    new Vector3(width,startY,zAxis),
+                    new Vector3(width,height, zAxis),
+                    new Vector3(startX,height,zAxis)
+
+                };
+
+
+            }
+            else
+            {
+                vertices = new Vector3[]
+                 {
+                    new Vector3(startX,startY,zAxis),
+                    new Vector3(startX,height,zAxis),
+                    new Vector3(width,height, zAxis),
+                    new Vector3(width,startY,zAxis)
+
+                 };
+
+
+            }
+        }
+
+        triangles = new int[] { 0, 1, 2, 0, 2, 3 };
+        // calculate the uvs at some point
+        shadowMesh.vertices = vertices;
+        shadowMesh.triangles = triangles;
+
+
+        shadowFilter.mesh = shadowMesh;
+        shadowMesh.RecalculateNormals();
+        shadowMesh.RecalculateBounds();
+
     }
 
 
@@ -79,19 +195,17 @@ public class DrawHitBox : MonoBehaviour
         float height = endPos.y;
         float width = endPos.x;
         
-        Debug.Log("Width and height is " + width + " "+ height);
+   
         float startX = startPos.x;
         float startY = startPos.y;
         //  in order to draw a plane we need 4 vertices. with 4 verticies we can make two right handed triangles and it is with those two right handed triangles we can draw a plane
 
 
-        // Due to something called a process called backwards culling items are only displayed if they triangles are created in the same order as vertices are inputted.
+       
         Vector3[] vertices; 
-      
-
-        
-
         int[] triangles; 
+        // Because of a process called backwards culling we need to make sure we feed in the vertices in a clockwise motion. So I get the velocity and height of the hitbox being drawn 
+        // and determine which way the numbers should be feed in 
         if (startY < height)
         {
            
@@ -105,7 +219,6 @@ public class DrawHitBox : MonoBehaviour
                     new Vector3(startX,height, zAxis),
                     new Vector3(width,height,zAxis),
                     new Vector3(width,startY,zAxis)
-
                };
                 
 
@@ -113,7 +226,7 @@ public class DrawHitBox : MonoBehaviour
             {
                 vertices = new Vector3[]
                 {
-                                // starting position of the vertices in world space
+                                
                     new Vector3(startX,startY, zAxis),
                     new Vector3(width,startY, zAxis),
                     new Vector3(width,height,zAxis),
@@ -123,11 +236,6 @@ public class DrawHitBox : MonoBehaviour
 
              
             }
-                
-
-
-
-           
 
             
         } else
