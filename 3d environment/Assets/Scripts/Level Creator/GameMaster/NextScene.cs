02cf09ab97db91c21fe5eb32ce84aaff90ce6a2d
ShadowGim <jacobcampbell972@gmail.com>
2020-04-18 23:53:51 +1000

    added basic vr headmovement to save scene

---
+++ 3d environment/Assets/Scripts/Level Creator/GameMaster/NextScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextScene : MonoBehaviour
+{
+
+    public float gameTime = 0f;
+
+
+    private void Update()
+    {
+        if (gameTime >20f)
+        {
+            SaveData.Save(); 
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+    }
+}
